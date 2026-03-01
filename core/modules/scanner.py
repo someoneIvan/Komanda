ff4020e08730b361f95d37c4d76f52e399a052e2
pleseinovivan <pleseinovivan@gmail.com>
2026-03-01 22:57:30 +0300

    Написан сканер

--- core/modules/scanner.py
+++ core/modules/scanner.py
@@ -1,20 +1,43 @@
 from pathlib import Path
 
 def scan(pathscan):
+
+    result = []
+
     if not (Path.exists and Path.is_dir):
         raise OSError("Error. Path not found.")
     else:
         try:
             for item in Path(pathscan).iterdir():
                 if item.is_file:
-                    pass
+                    namef = item.name
+                    sizef = item.stat().st_size
+                    formatf = item.suffix
+
+                    result.append({
+                        "name" : namef,
+                        "size" : sizef,
+                        "format" : formatf,
+                        "type" : "file"
+                    })
+
                 elif item.is_dir:
-                    pass
+                    named = item.name
+                    sized = item.stat().st_size
+                    formatd = ""
+                    
+                    result.append({
+                        "name" : named,
+                        "size" : sized,
+                        "format" : formatd,
+                        "type" : "directory"
+                    })
                 else:
-                    raise OSError("Unknown unique in directory.")
+                    raise OSError("Unknown item in directory.")
+            return result
         except Exception as e:
             return f"Unknown error: {e}"
 
 if __name__ == "__main__":
     pathscan = input(f"Test directory path: ")
-    scan(pathscan)
+    print(scan(pathscan))
