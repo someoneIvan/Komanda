afbe15778869e84c54920fc4e46a089ca7f73764
pleseinovivan <pleseinovivan@gmail.com>
2026-03-03 23:21:37 +0300

    Написал MVP механизма перемещения по директориям

--- core/actions.py
+++ core/actions.py
@@ -1,15 +1,18 @@
 import shutil
 from pathlib import Path
 
-def change_directory(new_location, current_path):
-    if new_location == ".":
-        pass
-    elif new_location == "..":
+def change_directory(almost_new_location, current_path):
+    almost_new_location = almost_new_location.strip()
+    if not almost_new_location or almost_new_location == ".":
+        return current_path
+    elif almost_new_location == "..":
         new_location = current_path.parent
+        return new_location
     else:
-        if Path.is_dir(new_location):
-            new_location = current_path / new_location.strip
-        elif Path.is_file(new_location):
-            print("This is file")
+        new_location = current_path / almost_new_location
+        if new_location.exists():
+            if new_location.is_dir():
+                return new_location
         else:
-            print("Unknown error")
+            print(f"Directory can't be changed. Path changing error.")
+            return current_path
